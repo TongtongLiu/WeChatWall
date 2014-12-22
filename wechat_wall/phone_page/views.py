4b30e611c6bcbdfdad5d1ef02354e72fae8d2db8
thsslimeng <thusw21lm@126.com>
2014-12-22 22:35:05 +0800

    haha

--- wechat_wall/phone_page/views.py
+++ wechat_wall/phone_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+
+def phone_page(requrst):
+    return render_to_response('phone_base.html')
