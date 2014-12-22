26138be25b4f08bd350f15c94783566dbd226a37
liutongtong <jshaltt7@163.com>
2014-12-22 22:05:46 +0800

    urls

--- wechat_wall/wechat_wall/urls.py
+++ wechat_wall/wechat_wall/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,4 +11,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^u/', include('phone_page.urls')),
     url(r'^', include('admin_page.urls')),
-) + staticfiles_urlpatterns()
+)
