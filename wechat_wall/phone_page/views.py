94b8d8525ba33c87dd41ca31c727a791e3b8fa27
liutongtong <jshaltt7@163.com>
2014-12-24 00:06:02 +0800

    lqbz

--- wechat_wall/phone_page/views.py
+++ wechat_wall/phone_page/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render_to_response
 
 def wall(request):
     return render_to_response('wall.html')
+
+
+def post_message(request):
+    if ((not request.POST) or
+            (not 'content' in request.POST) or
+            (not 'username' in request.POST) or
+            (not 'password' in request.POST)):
+        raise Http404
