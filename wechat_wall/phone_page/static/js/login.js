7ef1d8e17c71dbc36e044f96d2f8c986a4c9b1c5
thsslimeng <thusw21lm@126.com>
2014-12-25 03:53:36 +0800

    样式

--- wechat_wall/phone_page/static/js/login.js
+++ wechat_wall/phone_page/static/js/login.js
@@ -2,15 +2,7 @@
  * Created by limeng on 2014/12/25.
  */
 $(document).ready(function() {
-    var choose_clicked = false;
-    $('#choose').click(function () {
-        if (!choose_clicked) {
-            $('body').addClass('exposed');
-            choose_clicked = true;
-        }
-        else {
-            $('body').removeClass('exposed');
-            choose_clicked = false;
-        }
+    $('.example').click(function(){
+
     });
 });
