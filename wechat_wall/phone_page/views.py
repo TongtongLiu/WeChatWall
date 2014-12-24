dddf8373c3d14d178b2fedee66b4009a9e105ac8
thsslimeng <thusw21lm@126.com>
2014-12-25 05:16:30 +0800

    样式

--- wechat_wall/phone_page/views.py
+++ wechat_wall/phone_page/views.py
@@ -79,12 +79,12 @@ def select_old_messages_before_id(message_id, max_len):
 ######################## Date Operation End ###############################
 
 
-def loading(request, openid):
-    # code = request.GET.get('code')
-    # url = WEIXIN_URLS['get_openid'](code)
-    # res = http_get(url)
-    # rtn_json = json.loads(res)
-    # openid = rtn_json['openid']
+def loading(request):
+    code = request.GET.get('code')
+    url = WEIXIN_URLS['get_openid'](code)
+    res = http_get(url)
+    rtn_json = json.loads(res)
+    openid = rtn_json['openid']
     if select_users_by_openid(openid).exists():
         return redirect(s_reverse_wall(openid))
     else:
@@ -137,15 +137,15 @@ def login_register(request):
 
 
 def wall(request, openid):
-    users = select_users_by_openid(openid)
-    if not users:
-        return redirect(s_reverse_wall(openid))
-    user = users[0]
-    return render_to_response('wall.html',
-                              {'openid': openid, 'name': user.name, 'photo': user.photo},
-                              context_instance=RequestContext(request))
-    # return render_to_response('login.html', {'openid': openid, 'name': '管理员',
-    #                                         'photo': 'http://www.baidu.com/img/bd_logo1.png'})
+    # users = select_users_by_openid(openid)
+    # if not users:
+    #     return redirect(s_reverse_wall(openid))
+    # user = users[0]
+    # return render_to_response('wall.html',
+    #                           {'openid': openid, 'name': user.name, 'photo': user.photo},
+    #                           context_instance=RequestContext(request))
+    return render_to_response('login.html', {'openid': openid, 'name': '管理员',
+                                             'photo': 'http://www.baidu.com/img/bd_logo1.png'})
 
 
 @csrf_exempt
