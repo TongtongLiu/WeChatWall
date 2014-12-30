c0bfe058b65d77ba80d081d213287d257356d225
liutongtong <jshaltt7@163.com>
2014-12-30 22:05:36 +0800

    another wall

--- wechat_wall/phone_page/static/js/tt_wall.js
+++ wechat_wall/phone_page/static/js/tt_wall.js
@@ -3,5 +3,8 @@
  */
 
 $(document).ready(function() {
-    $('.content-wrap').css('height', document.height - $('.footer-position').css('height'));
+    var foot_height = $('.footer-position').css('height');
+    foot_height = parseInt(foot_height.substring(0, foot_height.length));
+    var body_height = (document.body.clientHeight - foot_height) + 'px';
+    $('.content-wrap').css('height', body_height);
 });
