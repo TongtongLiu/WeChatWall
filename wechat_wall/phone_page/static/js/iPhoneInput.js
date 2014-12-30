c92ba6b986fc879876fa32c3fb0ae2e2075a8093
thsslimeng <thusw21lm@126.com>
2014-12-30 11:20:31 +0800

    样式

--- wechat_wall/phone_page/static/js/iPhoneInput.js
+++ wechat_wall/phone_page/static/js/iPhoneInput.js
@@ -5,6 +5,8 @@
 //处理iPhone下fixed属性问题
 $(document).ready(function() {
     var sendBtn = $('.iPhone-send');
+
+    //监听输入框
     sendBtn.attr("disabled","disabled");
     sendBtn.css("color","rgba(235, 244, 235,0.5)");
     $('#iPhone-div-content').bind('input propertychange', function() {
@@ -18,26 +20,52 @@ $(document).ready(function() {
             sendBtn.css("color","rgba(235, 244, 235,1)");
         }
     });
+
+    //发送按钮点击事件
     sendBtn.click(function() {
-        var content = $('#iPhone-div-content').text(),
-        message = {
-            type: 'user_message',
-            content: content,
-            openid: openid
-        };
-        socket.send(message);
-
-        $('.wrap').css("display","block");
-        $('.iPhone-input').css("display","none");
-
+        var content = $('#iPhone-div-content').text();
+        sendBtn.attr("disabled","disabled");
+        sendBtn.css("color","rgba(235, 244, 235,0.5)");
         $('#iPhone-div-content').html("");
         sendBtn.attr("disabled","disabled");
         sendBtn.css("color","rgba(235, 244, 235,0.5)");
-        //跳转到页面底部
-        var height = $(document).height() - $('.footer').height();
-        $(window).scrollTop(height);
-        createDialog("prompt", "已发送");
+        $('.wrap').css("display","block");
+        $('.iPhone-input').css("display","none");
+
+        $.ajax({
+            url: $('#message_form').attr('action'),
+            type: "POST",
+            data: {
+                openid: openid,
+                content: content
+            },
+            success: function(data) {
+                switch (data) {
+                    case "BannedContent":
+                        createDialog("prompt", "小主不要乱说话喔o(╯□╰)o");
+                        break;
+
+                    case "Error":
+                        createDialog("warning", "出现了奇怪的错误~~(>_<)~~");
+                        break;
+
+                    case "Success":
+                        //滚动到页面底部
+                        $('body').animate({scrollTop: $(document).height()}, 1000);
+                        refreshImmediately();
+                        break;
+
+                    default:
+                        break;
+                }
+            },
+            error: function(data) {
+                console.info(data);
+            }
+        });
     });
+
+
     var browser = {
             versions: function () {
             var u = navigator.userAgent;
