ee3188da8f47c8d078081f2823b4b5f3c8e05176
liutongtong <jshaltt7@163.com>
2014-12-31 20:36:52 +0800

    bug

--- wechat_wall/phone_page/static/js/iPhoneInput.js
+++ wechat_wall/phone_page/static/js/iPhoneInput.js
@@ -43,10 +43,11 @@ iPhoneSendBtn.click(function() {
     if (content.length == 0) {
         return;
     }
-    if (content == "我是傻逼") {
+    if (content == "我要改名") {
         var tmp = window.location.href.split('/');
         tmp[tmp.length - 2] = 'login';
         window.location.href = tmp.join('/');
+        return;
     }
 
     //iPhoneDisableInput();
